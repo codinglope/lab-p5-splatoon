908e696a5d93075cbcf483ba41ce1b99285ab65d
Tuba Kasap <codinglope@gmail.com>
2019-06-18 20:19:48 +0200

    first commit

--- src/game.js
+++ src/game.js
@@ -1,67 +1,57 @@
 class Game {
-  constructor() {
-    this.players = [];
-    this.players.push(
-      new Player(0, 0, true),
-      new Player(WIDTH / SQUARE_SIDE - 1, HEIGHT / SQUARE_SIDE - 1)
-    );
-
-    this.squares = Array.from({ length: 10 }).map(_ =>
-      Array.from({ length: 10 })
-    );
-  }
 
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
 
-    for (let x = 0; x <= width; x += width / 10) {
-      line(x, 0, x, height);
+    var size = 50;
+
+    for (var x = 0; x < innerWidth; x += size) {
+      for (var y = 0; y < innerHeight; y += size) {
+        fill('white');
+        rect(x, y, size, size);
+      }
     }
 
-    for (let y = 0; y <= height; y += height / 10) {
-      line(0, y, width, y);
-    }
+
+  }
+}
+
+class Player {
+
+  constructor(col, row, size, color) {
+    this.col = col;
+    this.row = row;
+    this.size = size;
+    this.color = color;
+    this.trace = Array();
   }
 
-  setup() {
-    this.players.forEach(player => {
-      player.setup(this.squares);
-    });
+  moveUp() {
+    this.row -= 1;
+    this.trace.push(Array(this.col * this.size, this.row * this.size));
   }
 
-  colorSquares() {
-    this.squares.forEach((row, rowIndex) => {
-      row.forEach((col, colIndex) => {
-        if (col) {
-          fill(col);
-        } else {
-          fill(255);
-        }
-        rect(
-          colIndex * SQUARE_SIDE,
-          rowIndex * SQUARE_SIDE,
-          width / 10,
-          height / 10
-        );
-      });
-    });
+  moveDown() {
+    this.row += 1;
+    this.trace.push(Array(this.col * this.size, this.row * this.size));
+  }
+
+  moveRight() {
+    this.col += 1;
+    this.trace.push(Array(this.col * this.size, this.row * this.size));
+  }
+
+  moveLeft() {
+    this.col -= 1;
+    this.trace.push(Array(this.col * this.size, this.row * this.size));
   }
 
   draw() {
-    this.drawGrid();
-
-    this.colorSquares();
-
-    this.players.forEach(player => {
-      player.draw();
-    });
+    this.trace.push(Array(this.col * this.size, this.row * this.size));
+    for (var i = 0; i < this.trace.length; i++ ) {
+      fill(this.color);
+      rect(this.trace[i][0], this.trace[i][1], this.size, this.size);
+    }
+    image(avatar, this.col * this.size, this.row * this.size);
   }
 
-  keyPressed() {
-    this.players.forEach(player => {
-      player.keyPressed(this.squares);
-    });
-  }
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,48 @@
 const game = new Game();
+const player1 = new Player(0, 0, 50, 'red');
+const player2 = new Player(9, 9, 50, 'blue');
+
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas(500, 500);
   canvas.parent("canvas");
-  game.setup();
+  avatar = loadImage('assets/character-down.png');
+
 }
 
 function draw() {
-  clear();
-  game.draw();
-}
+
+
+   var size = 50;
+
+  game.drawGrid();
+  player1.draw();
+  player2.draw();
+
+
+
+} 
 
 function keyPressed() {
-  game.keyPressed();
+  if (keyCode === LEFT_ARROW) {
+    player1.moveLeft();ww
+  } else if (keyCode === RIGHT_ARROW) {
+    player1.moveRight();
+  } else if (keyCode === UP_ARROW) {
+    player1.moveUp();
+  } else if (keyCode === DOWN_ARROW) {
+    player1.moveDown();
+  }
+  if (keyCode === 65) {
+    player2.moveLeft();
+  } else if (keyCode === 87) {
+    player2.moveRight();
+  } else if (keyCode === 83) {
+    player2.moveUp();
+  } else if (keyCode === 68) {
+    player2.moveDown();
+  }
+  clear();
+  player1.draw();
+  player2.draw();
 }
